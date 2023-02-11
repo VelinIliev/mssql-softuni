cc432860b0adc963a1c811e1ae911c20c637cfa3
Velin Iliev <73158924+VelinIliev@users.noreply.github.com>
2023-02-11 17:44:35 +0200

    refactor

--- 07-FunctionsAndStoredProcedures/alltasks.sql
+++ 07-FunctionsAndStoredProcedures/alltasks.sql
@@ -201,4 +201,24 @@ GO
 
 USE Diablo
 
+GO
 
+CREATE OR ALTER FUNCTION ufn_CashInUsersGames(@gameName VARCHAR(50))
+RETURNS TABLE
+AS
+RETURN(
+	SELECT SUM(Cash) AS SumCash
+	FROM
+		(
+		SELECT Cash, ROW_NUMBER() OVER (ORDER BY Cash DESC) AS RowNumber
+		FROM UsersGames as ug
+		JOIN Games AS g
+		ON ug.GameId = g.Id
+		WHERE g.Name = @gameName
+		) AS t
+	WHERE RowNumber % 2 = 1
+	)
+
+GO
+
+SELECT * FROM ufn_CashInUsersGames('Love in a mist')
